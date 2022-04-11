9ce87741648a50f2b3b8ea5eb70a33a0fd20b069
Astronomax <fgfgfb93@gmail.com>
2022-04-11 03:56:59 +0300

    Add ClusteringView

---
+++ clustering/gui/ClusteringView.py
@@ -0,0 +1,47 @@
+import random
+from PyQt5.QtCore import Qt, QPointF, QRect
+from PyQt5.QtGui import QColor, QBrush, QPen
+from PyQt5.QtWidgets import QWidget, QGraphicsScene, QGraphicsView, QGraphicsItem
+
+
+class ClusteringView(QWidget):
+    def __init__(self, points: list[QPointF], target: list[int]):
+        super().__init__()
+        scene = QGraphicsScene()
+        self.points = points
+        self.target = target
+        self.graphicView = QGraphicsView(scene, self)
+        self.colors = dict((x, QColor(random.randint(1, 1000000000))) for x in list(set(target)))
+        self.graphicView.setScene(self.__getSceneWithPoints())
+        self.show()
+
+    def __getSceneWithPoints(self):
+        scene = QGraphicsScene()
+        points = self.__resizePoints()
+        for idx, point in enumerate(points):
+            scene.addEllipse(point.x(), point.y(), 10, 10,
+                             QPen(self.colors[self.target[idx]], 3, Qt.SolidLine),
+                             QBrush(self.colors[self.target[idx]])).setFlag(QGraphicsItem.ItemIsSelectable)
+        return scene
+
+    def __resizePoints(self):
+        minW = min(x.x() for x in self.points)
+        maxW = max(x.x() for x in self.points)
+        minH = min(x.y() for x in self.points)
+        maxH = max(x.y() for x in self.points)
+        width = self.graphicView.width()
+        height = self.graphicView.height()
+        k = min(width / (maxW - minW), height / (maxH - minH))
+        return (QPointF(k * (x.x() - minW), k * (x.y() - minH)) for x in self.points)
+
+    def wheelEvent(self, event):
+        self.graphicView.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        if event.angleDelta().y() > 0:
+            self.graphicView.scale(1.25, 1.25)
+        else:
+            self.graphicView.scale(0.8, 0.8)
+
+    def setGeometry(self, a0: QRect):
+        super().setGeometry(a0)
+        self.graphicView.setGeometry(0, 0, a0.width(), a0.height())
+        self.graphicView.setScene(self.__getSceneWithPoints())
