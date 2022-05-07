65f1e47ce1ccb30fbbb2b91d208ee67ce64ea6b8
Astronomax <fgfgfb93@gmail.com>
2022-05-07 17:06:56 +0300

    Fix bug

--- clustering/view/AlgoParamsSetter.py
+++ clustering/view/AlgoParamsSetter.py
@@ -1,4 +1,5 @@
 import uuid
+from copy import copy
 from dataclasses import dataclass
 from functools import partial
 
@@ -20,15 +21,15 @@ class ParamsSetterAttr:
 class AlgoParamsSetter(QWidget, DialogHelper):
     def __init__(self, attr: ParamsSetterAttr):
         super().__init__()
-        self.params: dict = attr.values
-        algo_params = attr.params
+        self.params: dict = copy(attr.values)
+        algo_params = copy(attr.params)
 
         layout = QVBoxLayout()
         for param in algo_params.int_params:
             line_edit = self.create_named_text_field(
                 name=param.name,
                 action=partial(self.change_int_param_value, param_name=param.name),
-                default_value=attr.values[param.name],
+                default_value=copy(self.params[param.name]),
                 validator=QIntValidator(),
                 set_title_horizontally=True
             )
@@ -38,7 +39,7 @@ class AlgoParamsSetter(QWidget, DialogHelper):
             box = QComboBox()
             box.addItems(param.items)
             box.currentTextChanged.connect(partial(self.change_selectable_param_value, param_name=param.name))
-            box.setCurrentIndex(param.items.index(attr.values[param.name]))
+            box.setCurrentIndex(param.items.index(copy(self.params[param.name])))
             self.change_selectable_param_value(box.currentText(), param.name)
             layout.addWidget(self.add_title_to_widget(param.name, box))
         self.setLayout(layout)
